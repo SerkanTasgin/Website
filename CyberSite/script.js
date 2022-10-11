3e6d87737d409a2100243c7b9f060313d6dd4b2b
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-11 09:46:29 +0200

    js added with write document

--- CyberSite/script.js
+++ CyberSite/script.js
@@ -1,4 +1,5 @@
-var birthYear = 1977;
-var futureYear  = 2098;
-var age  = futureYear - birthYear;
-console.log('I will be either ' + age + ' or ' + (age - 1) + ' in ' + futureYear);
+const birthYear = 1984;
+let futureYear = 2067;
+let ageBeforeBirthday = futureYear - birthYear;
+let ageAfterBirthday = ageBeforeBirthday - 1;
+document.write('I will be either ' + ageBeforeBirthday + ' or ' + ageAfterBirthday + ' in ' + futureYear);
