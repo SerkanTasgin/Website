a0111d0d850739a5b070f739e945322495a04692
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-11 11:26:49 +0200

    age and month calculator

--- CyberSite/script.js
+++ CyberSite/script.js
@@ -1,11 +1,13 @@
 const birthYear = 1984;
 let futureYear = 2045;
 let month = 6;
-let futureMonth = 12;
+let futureMonth = 1;
 let ageBeforeBirthday = futureYear - birthYear;
 let ageAfterBirthday = ageBeforeBirthday - 1;
+
 if(month >= futureMonth){
-console.log(ageAfterBirthday);
+    console.log('I will be ' + ageBeforeBirthday + ' in ' + futureYear);
+    document.write('I will be ' + ageBeforeBirthday + ' in ' + futureYear);
 }else{
     console.log(ageBeforeBirthday);
 }
