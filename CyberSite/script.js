8aab3f8f5af5f2c7bcb524e5d7592b1e3ee16392
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-11 10:12:50 +0200

    if and else and or

--- CyberSite/script.js
+++ CyberSite/script.js
@@ -1,4 +1,5 @@
 let myValue = 1;
+let myFirstString = "Hello World";
 
 if (myValue == 90){
     console.log("My value is equal to 10")
@@ -11,3 +12,12 @@ if(myValue > 100 || myValue == 100) {
     window.alert("Check wiht longer statement. My value is bigger or equal to 100")
 }else{window.alert("Check wiht longer statement. My value is smaller 100")
 }
+
+if(myFirstString == "Hello World") {
+    window.alert("Hello World");
+    document.write("Hello World");
+}else{window.alert("Not saying Hello World");
+
+
+}
+
