112b463084a0e04ace4b320d397e71c08b66fd70
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-11 09:58:23 +0200

    first if statement

--- CyberSite/script.js
+++ CyberSite/script.js
@@ -1,5 +1,12 @@
-let myValue = 10;
+let myValue = 100;
 
 if (myValue == 90){
     console.log("My value is equal to 10")
+}
+if(myValue >= 100){
+    window.alert("My value is bigger or equal to 100")
+
+}
+if(myValue > 100 || myValue == 100) {
+    window.alert("Check wiht longer statement. My value is bigger or equal to 100")
 }
