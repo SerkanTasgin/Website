64bf75a09d8af8d12372ee2b51d874d516acf239
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-14 15:08:02 +0200

    js more

--- Project/project.js
+++ Project/project.js
@@ -7,27 +7,50 @@ let container = document.getElementById("container");
 
 
 let people = [
-    { name: "Serkan", age: 45, image: "images/21-serkan.jpg" },
-    { name: "Baris", age: 45, image: "images/baris.jpg" } ,
-    { name: "Enes", age: 35, image: "images/19-enes.jpg" },
-    { name: "Hasan", age: 41, image: "images/09-hasan.jpg" },
-    { name: "Ali", age: 35, image: "images/ali.jpg" },
-    { name: "Senay", age: 35, image: "images/35-senay.jpg" },
-    { name: "Fatih", age: 35, image: "images/31-fatih.jpg" },
-    { name: "Jyothi", age: 35, image: "images/jyothi.jpg" },
-    { name: "Ari", age: 35, image: "images/25-ari.jpg" }
+    { name: "Serkan", Country: "Turkey", image: "images/21-serkan.jpg" },
+    { name: "Baris", Country: "Turkey", image: "images/baris.jpg" } ,
+    { name: "Enes", Country: "Turkey", image: "images/19-enes.jpg" },
+    { name: "Hasan", Country: "Turkey", image: "images/09-hasan.jpg" },
+    { name: "Ali", Country: "Turkey", image: "images/ali.jpg" },
+    { name: "Senay", Country: "Turkey", image: "images/35-senay.jpg" },
+    { name: "Fatih", Country: "Turkey", image: "images/31-fatih.jpg" },
+    { name: "Jyothi", Country: "India", image: "images/jyothi.jpg" },
+    { name: "Ari", Country: "Indonesia", image: "images/25-ari.jpg" },
+    { name: "Murat", Country: "Turkey", image: "images/murat.jpg" },
+    { name: "Yusuf", Country: "Turkey", image: "images/yusuf.jpg" },
+    { name: "ismet", Country: "Turkey", image: "images/ismet.jpg" },
+    { name: "Tuba", Country: "Turkey", image: "images/tuba.jpg" },
+    { name: "Olena", Country: "Ukraine", image: "images/olena.jpg" },
+    { name: "Oguz", Country: "Turkey", image: "images/oguz.jpg" },
+    { name: "Bilal", Country: "Afghanistan", image: "images/bilal.jpg" },
+    { name: "ibrahim", Country: "Turkey", image: "images/ibrahim.jpg" },
+    { name: "Mustafa", Country: "Turkey", image: "images/06-mustafa.jpg" },
+    { name: "Shamil", Country: "Russia", image: "images/30-shamil.jpg" },
+    { name: "Baris", Country: "Turkey", image: "images/baris.jpg" },
+    { name: "Sadettin", Country: "Turkey", image: "images/sadettin.jpg" },
+    { name: "Yulia", Country: "Ukraine", image: "images/05-yulia.jpg" },
+    { name: "Abdulfettah", Country: "Afghanistan", image: "images/abdulfettah.jpg" },
+    { name: "Arwa", Country: "Syria", image: "images/37-arwa.jpg" }
 ]
+
+function clicked() {
+    console.log("first try")
+}
 
 let str = "";
 for (let i = 0; i < people.length; i++) {
     let person = people[i];
-    str += "<div>"
+    str += "<div class=\"card\" onClick=\"clicked()\">"
     str += '<img src="' + person.image + '">'
+    str += "<div class=\"details\">"
     str += "<p>" + person.name + "</p>"
-    str += "<p>" + person.age + "</p>"
+    str += "<p>" + person.Country + "</p>"
+    str += "</div>"
     str += "</div>"
 
+
     }
+ 
 container.innerHTML = str;
 
 
