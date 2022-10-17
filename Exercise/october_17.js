2bf010a6bcd225520bd26afd2bf57b5b6f19ed8f
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-17 15:34:13 +0200

    comparing numbers with js

--- Exercise/october_17.js
+++ Exercise/october_17.js
@@ -2,14 +2,20 @@ let num1 = prompt ("Plearse enter the first number");
 let num2 = prompt ("please enter the second number");
 
 
-
-let larger;
-if (num1 >= num2){
-
-    bigger = num1;
+if (isNaN (num1)||isNaN (num2)){
+    alert("please enter number")
 }
-else ( num2 >= num1)
-    bigger = num2;
+else {
 
     
-alert("The larger number is " + bigger);
+    if (num1 >= num2){
+    
+        bigger = num1;
+    }
+    else ( num2 >= num1)
+        bigger = num2;
+    
+        
+    alert("The larger number is " + bigger);
+
+}
