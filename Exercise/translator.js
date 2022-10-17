aaa73f4c6fbfaf4ccafb7b16608d65f8b0bf081c
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-17 15:50:46 +0200

    comparing numbers with js

---
+++ Exercise/translator.js
@@ -0,0 +1,13 @@
+function helloWorld(lang) {
+    if (lang == 'fr') {
+        return 'Bonjour tout le monde';
+    } else if (lang == 'es') {
+        return 'Hola, Mundo';
+    } else {
+        return 'Hello, World';
+    }
+}
+
+alert(helloWorld('en'));
+alert(helloWorld('fr'));
+alert(helloWorld('es'));
