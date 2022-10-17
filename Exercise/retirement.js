705bd15bd1bb05981a286c7513636f023f1ea692
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-17 16:26:13 +0200

    retirement calculator

---
+++ Exercise/retirement.js
@@ -0,0 +1,5 @@
+let ageCurrent = prompt ("Please enter your age");
+let ageRetirement = prompt ("please enter the age you want to retire");
+let yearCurrent = 2022;
+let yearRetirement = yearCurrent + (ageRetirement - ageCurrent);
+alert(' You are ' + ageCurrent + ' years old. ' + ' You will be retired at ' + ageRetirement + ' It is ' + yearCurrent + ' so you can retire in ' + yearRetirement)
