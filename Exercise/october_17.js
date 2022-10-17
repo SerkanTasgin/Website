32ed3dda6301cfbeeab8173327a9bb33487537b6
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-17 14:25:01 +0200

    comparing numbers with js

---
+++ Exercise/october_17.js
@@ -0,0 +1,10 @@
+let number1 = "5";
+let number2 = "10";
+
+if (number1 <= number2) {
+    document.write('The greater number of'  + number1  + 'and' + number2  + 'is' + number2);
+} else {
+    document.write('The smaller number of'  + number1  + 'and' + number2  + 'is' + number1);
+}
+   
+
