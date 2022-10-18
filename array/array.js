cf525dd0b0e060284f1086e9e2055013b5f3e7ea
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-18 09:58:14 +0200

    array example

---
+++ array/array.js
@@ -0,0 +1,18 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+document.write(array[2]);
+
+
+array.shift(0);
+alert(array);
+
+array.sort();
+
+array.push("Kiwi");
+alert(array);
+
+array.shift(0);
+
+array.reverse();
+alert(array);
+
+
