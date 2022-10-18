79af2a28419ad0b9e7dfd3bb72c21b7fbf123cb4
SerkanTasgin <serkan.tasgin@powercoders.org>
2022-10-18 12:07:13 +0200

    array with objects

---
+++ array/recipe.js
@@ -0,0 +1,12 @@
+let person = {
+    name:"NinjaGo",
+    number:5,
+    characters: ["Kai", "Nia", "Jay", "Lloyd", "Cole", "Zane"] 
+}
+
+console.log("Name of the Movie:  " + person.name);
+console.log("Number of Characters: " + person.number);
+console.log("Movie Characters: " + person.characters);
+
+
+
