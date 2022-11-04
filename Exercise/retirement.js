89f6584626283b4cc1fbf4f07668c5e85f0645dd
SerkanTasgin <114902724+SerkanTasgin@users.noreply.github.com>
2022-11-04 14:51:24 +0100

    Delete Exercise directory

--- Exercise/retirement.js
+++
@@ -1,6 +0,0 @@
-let ageCurrent = prompt ("Please enter your age");
-let ageRetirement = prompt ("please enter the age you want to retire");
-let yearCurrent = 2022;
-let yearLeft = (ageRetirement - ageCurrent);
-let yearRetirement = yearCurrent + (ageRetirement - ageCurrent);
-alert(' You have ' + yearLeft + ' years left until you can retire. It is ' + yearCurrent + ' so you can retire in ' + yearRetirement)
